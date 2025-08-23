279a26a1b6e0e98f9f6d60a9b2d5a3307b5f6f62
Daniel Kotsugai <dkotsugai@gmail.com>
2025-08-23 07:37:20 -0300

    add initial project files and configuration for Spring Boot application

---
+++ projeto2/pastelFatec2/src/main/java/com/fatec/sjc/pastelFatec2/PastelFatec2Application.java
@@ -0,0 +1,13 @@
+package com.fatec.sjc.pastelFatec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastelFatec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastelFatec2Application.class, args);
+	}
+
+}
---
+++ projeto2/pastelFatec2/src/test/java/com/fatec/sjc/pastelFatec2/PastelFatec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.sjc.pastelFatec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastelFatec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

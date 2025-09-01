d2f81135e07d94a033f8cf863943a67a7ed85634
Daniel Kotsugai <dkotsugai@gmail.com>
2025-09-01 01:53:39 -0300

    initialize PastelFatec3 project with Maven setup and Spring Boot configuration

---
+++ projeto3/pastelFatec3/pastelFatec3/src/main/java/com/fatec/sjc/pastelFatec3/PastelFatec3Application.java
@@ -0,0 +1,13 @@
+package com.fatec.sjc.pastelFatec3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastelFatec3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastelFatec3Application.class, args);
+	}
+
+}
